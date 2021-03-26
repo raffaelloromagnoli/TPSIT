b6f7d06d800acd56a04f254e886811b4e006c0b0
Raffaello Romagnoli <71867642+raffaelloromagnoli@users.noreply.github.com>
2021-03-26 17:38:40 +0100

    Add files via upload

---
+++ ES010/cmake-build-debug/CMakeFiles/ES010.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ES010.dir/main.c.obj"
+  "ES010.exe"
+  "ES010.exe.manifest"
+  "ES010.pdb"
+  "libES010.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ES010.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
