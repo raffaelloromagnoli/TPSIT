ca34762ce8625ca651f2c77cb3a5f1831980be9d
Raffaello Romagnoli <71867642+raffaelloromagnoli@users.noreply.github.com>
2021-03-26 17:41:00 +0100

    Add files via upload

---
+++ ES012_Pila/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void enqueue(struct queue_node **head,
+        struct queue_node **tail, struct  queue_node **element){
+    if (is_empty)
+}
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
