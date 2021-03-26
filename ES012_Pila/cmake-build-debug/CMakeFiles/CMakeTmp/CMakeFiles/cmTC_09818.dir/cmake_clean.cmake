9686a1bb41392ecf24b92e9c35164de14f56aad6
Raffaello Romagnoli <71867642+raffaelloromagnoli@users.noreply.github.com>
2021-03-26 17:39:58 +0100

    Add files via upload

---
+++ ES012_Pila/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_09818.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_09818.dir/testCCompiler.c.obj"
+  "cmTC_09818.exe"
+  "cmTC_09818.exe.manifest"
+  "cmTC_09818.pdb"
+  "libcmTC_09818.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_09818.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ES012_Pila/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_09818.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_09818.
+# This may be replaced when dependencies are built.
