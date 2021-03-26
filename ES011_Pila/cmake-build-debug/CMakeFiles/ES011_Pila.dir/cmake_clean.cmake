7ae13180c978f96bbdbc1645a8e6cc841728da22
Raffaello Romagnoli <71867642+raffaelloromagnoli@users.noreply.github.com>
2021-03-26 17:39:20 +0100

    Add files via upload

---
+++ ES011_Pila/cmake-build-debug/CMakeFiles/ES011_Pila.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ES011_Pila.dir/main.c.obj"
+  "ES011_Pila.exe"
+  "ES011_Pila.exe.manifest"
+  "ES011_Pila.pdb"
+  "libES011_Pila.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ES011_Pila.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ES011_Pila/cmake-build-debug/CMakeFiles/ES011_Pila.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ES011_Pila.
+# This may be replaced when dependencies are built.
